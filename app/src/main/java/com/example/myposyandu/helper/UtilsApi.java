04e737f2d59688f63e7b846d358f45dba5b88994
dhanty12@gmail.com <dhanty12@gmail.com>
2020-07-26 12:23:39 +0700

    tambah bayi

--- app/src/main/java/com/example/myposyandu/helper/UtilsApi.java
+++ app/src/main/java/com/example/myposyandu/helper/UtilsApi.java
@@ -4,7 +4,7 @@ package com.example.myposyandu.helper;
 public class UtilsApi {
     // 10.0.2.2 ini adalah localhost.
     // bisa juga di masukan dengan IP address kalian
-//    public static final String BASE_URL_API = "http://192.168.1.10/Api_Android/";
+//    public static final String BASE_URL_API = "http://192.168.1.4/Api_Android/";
     public static final String BASE_URL_API = "http://192.168.43.216/Api_Android/";
 
     // Mendeklarasikan Interface BaseApiService
