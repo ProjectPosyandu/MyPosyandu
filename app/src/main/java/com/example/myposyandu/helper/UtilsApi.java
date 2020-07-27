b4a7e468743bc84423c6612109f29c770d7396fd
MARPC\Mardiana <now101246@gmail.com>
2020-07-27 19:11:42 +0700

    x

--- app/src/main/java/com/example/myposyandu/helper/UtilsApi.java
+++ app/src/main/java/com/example/myposyandu/helper/UtilsApi.java
@@ -4,14 +4,11 @@ package com.example.myposyandu.helper;
 public class UtilsApi {
     // 10.0.2.2 ini adalah localhost.
     // bisa juga di masukan dengan IP address kalian
-<<<<<<< HEAD
 //    public static final String BASE_URL_API = "http://192.168.1.10/Api_Android/";
 //    public static final String BASE_URL_API = "http://192.168.43.216/Api_Android/";
     public static final String BASE_URL_API = "http://192.168.43.36/Api_Android/";
-=======
 //    public static final String BASE_URL_API = "http://192.168.1.4/Api_Android/";
-    public static final String BASE_URL_API = "http://192.168.43.216/Api_Android/";
->>>>>>> 04e737f2d59688f63e7b846d358f45dba5b88994
+//    public static final String BASE_URL_API = "http://192.168.43.216/Api_Android/";
 
     // Mendeklarasikan Interface BaseApiService
     public static ApiService getAPIService() {
