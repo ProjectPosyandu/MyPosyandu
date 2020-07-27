2e416b2aff394d63882a93673033a7e486c12d55
MARPC\Mardiana <now101246@gmail.com>
2020-07-27 19:53:04 +0700

    ganti

--- app/src/main/java/com/example/myposyandu/ui/ArtikelFragment.java
+++ app/src/main/java/com/example/myposyandu/ui/ArtikelFragment.java
@@ -7,19 +7,17 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myposyandu.R;
 
-public class HomeFragment extends Fragment {
+public class ArtikelFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        View root = inflater.inflate(R.layout.fragment_artikel, container, false);
+        final TextView textView = root.findViewById(R.id.text_slideshow);
+        textView.setText("Disini artikel");
 
         return root;
     }
--- app/src/main/java/com/example/myposyandu/ui/JadwalFragment.java
+++ app/src/main/java/com/example/myposyandu/ui/JadwalFragment.java
@@ -7,22 +7,17 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.myposyandu.R;
 
-public class GalleryFragment extends Fragment {
+public class JadwalFragment extends Fragment {
 
      public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        View root = inflater.inflate(R.layout.fragment_jadwal, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
-
-
-        
+        textView.setText("Disini jadwal");
         return root;
     }
 }
