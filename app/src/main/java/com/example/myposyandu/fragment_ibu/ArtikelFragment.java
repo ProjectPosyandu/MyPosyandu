87442433484fdbe9fc4fead471707d24464c9496
MARPC\Mardiana <now101246@gmail.com>
2020-08-03 14:56:38 +0700

    kirim pesan di jadwal imunisasi

--- app/src/main/java/com/example/myposyandu/fragment_ibu/ArtikelFragment.java
+++ app/src/main/java/com/example/myposyandu/fragment_ibu/ArtikelFragment.java
@@ -9,23 +9,6 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.myposyandu.R;
-import com.example.myposyandu.helper.ApiService;
-import com.example.myposyandu.helper.RetrofitClient;
-import com.example.myposyandu.helper.UtilsApi;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ArtikelFragment extends Fragment {
 
